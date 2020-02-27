1d1d1680d96dac73477eda45bde9999a4bdefccd
sa <sa@wrf.su>
2020-02-27 20:19:37 +0300

    Initial Commit

---
+++ RGBSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGBSlider
+//
+//  Created by lastbyte on 27.02.2020.
+//  Copyright © 2020 lastbyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
